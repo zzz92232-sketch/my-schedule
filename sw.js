abb4f5df0faa2f265514adeb38806f88aa8f2cf0
zzz92232-sketch <zzz92232@gmail.com>
2026-06-24 19:39:02 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'schedule-app-v1';
+const ASSETS = [
+  'schedule.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
